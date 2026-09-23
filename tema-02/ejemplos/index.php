123d2469b2631a58da399412e3bf3374f7975465
David <dcarjim0101@g.educaand.es>
2026-09-23 12:19:32 +0200

    si

---
+++ tema-02/ejemplos/index.php
@@ -0,0 +1,13 @@
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hola mundo PHP</title>
+</head>
+<body>
+    <h1>Hola, Mundo!</h1>
+    <?php
+        echo "<p>Hola Mundo desde PHP</p>";
+    ?>
+</body>
+</html>
