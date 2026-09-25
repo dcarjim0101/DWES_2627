8dd69f026c6681d267cc046018a1e67634b5090b
David <dcarjim0101@g.educaand.es>
2026-09-25 13:08:05 +0200

    tema 2

---
+++ tema-02/actividades/01/01/index.php
@@ -0,0 +1,28 @@
+/*
+    Actividad_211_06
+    Tema 2.
+    DWES 26/27
+    Alumno: David Carrero Jiménez
+*/
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 213_06</title>
+</head>
+
+<body>
+
+    <!-- titulo, parrafos, enlace -->
+
+    <?php
+        echo "<h1>Hola Mundo</h1>";
+        echo "<p>este es un parrafo de al menos 3 lineas</p>";
+        echo "<p>esta es la segunda linea del parrafo</p>";
+        echo "<p>y esta es la tercera linea del parrafo</p>";
+        echo "<a href='http://www.elpais.es'>ir a la paginas web de elpais</a>";
+    ?>
+
+</body>
+</html>
---
+++ tema-02/actividades/01/02/index.php
@@ -0,0 +1,31 @@
+/*
+    Actividad_212_06
+    Tema 2.
+    DWES 26/27
+    Alumno: David Carrero Jiménez
+*/
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 213_06</title>
+</head>
+
+<body>
+
+    <!-- titulo, parrafos, enlace -->
+
+    <?php
+        echo "<h1>Hola Mundo</h1>";
+        echo "<p>este es un parrafo de al menos 3 lineas</p>";
+        echo "<p>esta es la segunda linea del parrafo</p>";
+        echo "<p>y esta es la tercera linea del parrafo</p>";
+        echo "<a href='http://www.elpais.es'>ir a la paginas web de elpais</a>";
+    ?>
+
+    <!-- imagen -->
+    <img src="https://www.elpais.com/resizer/0J1g59g0k1J5Z8x6z3F7G8F5G8=/1200x0/filters:focal(0x0:1200x800):format(webp)/cloudfront-eu-central-1.images.arcpublishing.com/prisa/2Q4XK6Y3V5E4ZL7W2X5Y6Z7Y6A.jpg" alt="Imagen">
+
+</body>
+</html>
